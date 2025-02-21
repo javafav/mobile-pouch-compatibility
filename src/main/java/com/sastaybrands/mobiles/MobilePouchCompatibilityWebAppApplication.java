7bdb138ff6db2d317b32ccd4f3241ca690784c23
sanykhan <usmanshj3@gmail.com>
2025-02-21 21:06:57 +0500

    Initial commit: Set up Mobile Compatibility Web App with Spring Boot and Thymeleaf

---
+++ src/main/java/com/sastaybrands/mobiles/MobilePouchCompatibilityWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.sastaybrands.mobiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobilePouchCompatibilityWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobilePouchCompatibilityWebAppApplication.class, args);
+	}
+
+}
